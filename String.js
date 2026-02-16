3227886c35ebd4752fe4d9a9cba4cd4bf7d11c29
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-16 12:02:11 +0600

    string data type

---
+++ String.js
@@ -0,0 +1,7 @@
+var a = 'Hello';
+var b = 20;
+var c = "Hello ".concat(b);
+// console.log(c)
+var num = 30;
+var str = num.toString(); // string to number converted
+var str2 = str + ''; // string to number converted
---
+++ String.ts
@@ -0,0 +1,9 @@
+var a: string = 'Hello';
+var b:number = 20;
+var c:string = `Hello ${b}`;
+
+// console.log(c)
+
+var num:number = 30;
+var str:string = num.toString(); // string to number converted
+var str2:string = str + ''; // string to number converted
