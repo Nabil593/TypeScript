3505e30543aa4b29ef2c285252911e995889645e
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-17 18:57:50 +0600

    Any data type

---
+++ Any.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 'nabil'; // Any type can hold any value and can be reassigned to any type of value.
+a = 10;
+a = true;
+a = [1, 2, 3];
+a = { name: "Stark", age: 30 };
+console.log(a);
