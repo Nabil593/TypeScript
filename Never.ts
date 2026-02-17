170618a77be3b6d888bae65bcc7d626423171ab6
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-17 19:36:42 +0600

    Never data type

---
+++ Never.ts
@@ -0,0 +1,10 @@
+function never(): never {
+    while(true) {
+        console.log('Go..');
+    }
+} // This function type is never, which means it will never returns anything, it will never end. It is used for function that always throws on error or functions that never return.
+
+
+// function test(): never {
+//     console.log('End')
+// } // this will give an error because the function is ending without returning anything, it is not a never type function.
