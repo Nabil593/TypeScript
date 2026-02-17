b4d25e5b4b03555d76a8dec43105c31bf5e80fc8
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-17 20:33:29 +0600

    FunctionParams type

---
+++ FunctionParams.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sum(a, b, c) {
+    return a + b;
+}
+console.log(sum(2, 3));
---
+++ FunctionParams.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b: number, c?: string ): number {
+    return a + b;
+}
+
+console.log(sum(2, 3));
