f8d489607f63ee958ee1705e166fb95426fc9647
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-20 14:58:34 +0600

    Custom Type

---
+++ Type.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const a = {
+    name: 'Shariea',
+    age: 19
+};
+console.log(a);
---
+++ Type.ts
@@ -0,0 +1,8 @@
+type aType = {name: string, age: number}; // Custom type.
+
+const a: aType= {
+    name: 'Shariea',
+    age: 19
+};
+
+console.log(a);
