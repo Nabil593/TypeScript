2216246833eb4581dba095c4583ec7d0b86a67c5
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-21 11:44:32 +0600

    Dom in Typescript

--- Dom.js
+++ Dom.js
@@ -1,2 +1,9 @@
-console.log('Doom');
+const f = document.querySelector('h1');
+console.log(f.textContent);
+const c = document.querySelector('h1');
+const d = document.querySelector('a');
+const anchor = document.querySelector('.anchor-style');
+console.log(anchor.href);
+console.log(d?.href);
+console.log(f?.classList);
 export {};
--- Dom.ts
+++ Dom.ts
@@ -1,1 +1,10 @@
-console.log('Doom');
+const f = document.querySelector('h1')!;
+console.log(f.textContent);
+
+const c = document.querySelector('h1');
+const d = document.querySelector('a');
+const anchor = document.querySelector('.anchor-style')! as HTMLAnchorElement;
+
+console.log(anchor.href);
+console.log(d?.href);
+console.log(f?.classList);
